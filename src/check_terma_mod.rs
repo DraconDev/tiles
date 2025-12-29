d63aa1bfed22ee960d3eac15ee74f86eba26137d
DraconDev <dracsharp@gmail.com>
2025-12-29 06:53:42 +0000

    Update mouse event handling in check_terma_mod
    - Rename mouse event fields from (col, row) to (column, line)
    - Remove unused key event handling code
    - Add missing newline at end of file

--- src/check_terma_mod.rs
+++ src/check_terma_mod.rs
@@ -1,24 +1,17 @@
+
 use terma::input::parser::{Event, MouseButton, KeyCode};
 use terma::integration::ratatui::TermaBackend;
 
 pub fn check() {
     let event: Event = unsafe { std::mem::zeroed() };
     match event {
-        Event::Mouse { button, col, row, .. } => {
+        Event::Mouse { button, column, line, .. } => {
             match button {
                 MouseButton::Left => {},
                 MouseButton::Right => {},
                 MouseButton::Middle => {},
                 MouseButton::Back => {},
                 MouseButton::Forward => {},
-                _ => {},
-            }
-        }
-        Event::Key(key) => {
-            let code = key.code;
-            match code {
-                KeyCode::Char(_) => {},
-                KeyCode::Enter => {},
                 _ => {},
             }
         }
