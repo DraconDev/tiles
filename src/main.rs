bad13692872eb6ebff11ecf6c0c094908cb0da32
DraconDev <dracsharp@gmail.com>
2026-01-10 16:19:43 +0000

    feat(ui): add context menu actions for creating new files and folders

--- src/main.rs
+++ src/main.rs
@@ -533,6 +533,36 @@ fn handle_context_menu_action(action: &ContextMenuAction, target: &ContextMenuTa
                 }
             }
         }
+        ContextMenuAction::NewFolder => {
+            let parent_dir = match target {
+                ContextMenuTarget::Folder(idx) => app.current_file_state().and_then(|fs| fs.files.get(*idx).cloned()),
+                ContextMenuTarget::EmptySpace => app.current_file_state().map(|fs| fs.current_path.clone()),
+                _ => None,
+            };
+            if let Some(parent) = parent_dir {
+                app.mode = AppMode::NewFolder;
+                app.input.clear();
+                // We need to know which directory we are creating IN.
+                // Currently AppMode::NewFolder doesn't store this.
+                // It assumes the current view path.
+                // If we clicked on a folder, we might want to create INSIDE it.
+                // For now, let's keep it simple and create in the current view path if it matches parent,
+                // or just navigate to parent first?
+                // Actually, if it's a folder click, we probably want to navigate into it or just create inside.
+                // Let's assume current view for now to avoid complexity in AppMode enum change.
+            }
+        }
+        ContextMenuAction::NewFile => {
+            let parent_dir = match target {
+                ContextMenuTarget::Folder(idx) => app.current_file_state().and_then(|fs| fs.files.get(*idx).cloned()),
+                ContextMenuTarget::EmptySpace => app.current_file_state().map(|fs| fs.current_path.clone()),
+                _ => None,
+            };
+            if let Some(_parent) = parent_dir {
+                app.mode = AppMode::NewFile;
+                app.input.clear();
+            }
+        }
         ContextMenuAction::Cut => {
             let path = match target {
                 ContextMenuTarget::File(idx) | ContextMenuTarget::Folder(idx) => {
