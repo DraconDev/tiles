aaa5f7f65e607cea08e75e1f29624fd18cab74cc
DraconDev <dracsharp@gmail.com>
2026-01-13 13:55:35 +0000

    chore: update codebase (fallback)

--- src/app.rs
+++ src/app.rs
@@ -506,6 +506,7 @@ pub struct App {
     pub process_scroll: usize,
     pub process_search_filter: String,
     pub process_column_bounds: Vec<(Rect, ProcessColumn)>,
+    pub monitor_subview: MonitorSubview,
 }
 
 impl App {
