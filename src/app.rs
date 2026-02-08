b4082bca41a65a63d7bfae058af9b96517e6fc2a
DraconDev <dracsharp@gmail.com>
2026-02-08 22:57:48 +0000

    feat: update app.rs

--- src/app.rs
+++ src/app.rs
@@ -99,6 +99,7 @@ pub struct App {
     #[allow(dead_code)]
     pub tile_queue: Arc<Mutex<Vec<TilePlacement>>>,
     pub saved_pane: Option<Pane>,
+    pub return_to_git_after_editor: bool,
 }
 
 impl App {
@@ -253,6 +254,7 @@ impl App {
             background_tasks: Vec::new(),
             tile_queue,
             saved_pane: None,
+            return_to_git_after_editor: false,
         }
     }
 
