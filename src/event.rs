88ed5c4ba914ac5e90fe0f3bac202e39203c1e0b
DraconDev <dracsharp@gmail.com>
2026-03-17 22:47:51 +0000

    chore(misc): wip checkpoint --- category: chore scope: misc ---

--- src/event.rs
+++ src/event.rs
@@ -1,5 +1,5 @@
-use dracon_terminal_engine::contracts::Event;
 use dracon_terminal_engine::input::event::Event as RuntimeEvent;
+use dracon_terminal_engine::input::mapping::Event;
 
 pub fn convert_event(evt: RuntimeEvent) -> Option<Event> {
     Some(dracon_terminal_engine::input::mapping::from_runtime_event(
